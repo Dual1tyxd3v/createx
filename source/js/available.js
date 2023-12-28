94adb8d76ab3509abf27d1b5e3d62d05c09929ea
Dual1tyxd3v <d3vilc0re@gmail.com>
2023-12-29 00:43:22 +0700

    Finished available page

--- source/js/available.js
+++ source/js/available.js
@@ -1,18 +1,33 @@
-// Subscribe modal
+// Modal window
 const btnSubscribe = document.querySelector('.js-subscribe');
 const modal = document.querySelector('.modal');
 const modalContainer = document.querySelector('.modal__container');
 const btnCloseModal = document.querySelector('.btn-close');
 const susbscribeForm = document.querySelector('.modal__subscribe');
+const cvForm = document.querySelector('.modal__cv');
+const btnSendCV = document.querySelector('.js-sendCV')
 
-btnSubscribe.addEventListener('click', () => {
-  modalContainer.innerHTML = '';
-  const form = susbscribeForm.cloneNode(true);
-  form.classList.remove('hide');
-  modalContainer.append(form);
-  modal.classList.remove('hide');
-});
+btnSubscribe.addEventListener('click', () => showModal(susbscribeForm));
 
 btnCloseModal.addEventListener('click', () => {
   modal.classList.add('hide');
+  modalContainer.innerHTML = '';
 });
+
+btnSendCV.addEventListener('click', () => showModal(cvForm));
+
+function showModal(template) {
+  const form = template.cloneNode(true);
+  form.classList.remove('hide');
+  modalContainer.append(form);
+  modal.classList.remove('hide');
+
+  if (!form.classList.contains('modal__cv')) return;
+
+  const inputFile = form.querySelector('.form__file');
+  const labelFile = form.querySelector('.form__label--file');
+
+  inputFile.addEventListener('change', (e) => {
+    labelFile.innerHTML = `Attach your CV* -&nbsp;<span class="form__label--active"> (${e.target.files[0].name})</span>`;
+  });
+}
